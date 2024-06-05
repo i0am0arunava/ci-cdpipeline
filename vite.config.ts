c3ba7b3dc91ba823d050a1867c88393ac7524d31
eat_code_sleep <141677292+i0am0arunava@users.noreply.github.com>
2024-06-05 15:56:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cicd/",
+  base: "/ci-cdpipeline/",
   plugins: [
     react(),
    
